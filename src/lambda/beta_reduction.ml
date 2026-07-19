88883b29d208fb0bbe29e913a1820dc8ce0dd2a8
krishnasys-admin <306674743+krishnasys-admin@users.noreply.github.com>
2026-07-19 19:09:34 +0300

    Improve beta reduction recursion

--- src/lambda/beta_reduction.ml
+++ src/lambda/beta_reduction.ml
@@ -1,7 +1,25 @@
-let reduce term =
+let rec reduce term =
   match term with
   | Term.Application (Term.Lambda (identifier, body), argument) ->
       Some (Substitution.substitute identifier argument body)
 
-  | _ ->
+  | Term.Application (left, right) -> (
+      match reduce left with
+      | Some left' ->
+          Some (Term.apply left' right)
+      | None ->
+          match reduce right with
+          | Some right' ->
+              Some (Term.apply left right')
+          | None ->
+              None )
+
+  | Term.Lambda (identifier, body) -> (
+      match reduce body with
+      | Some body' ->
+          Some (Term.lambda_id identifier body')
+      | None ->
+          None )
+
+  | Term.Variable _ ->
       None
